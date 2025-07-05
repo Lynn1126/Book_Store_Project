2a33427013491b06bd36d0f9fd585f2da0effaa9
Lynn <421666614@qq.com>
2025-07-04 17:45:20 -0700

    Added banner part.

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -2,6 +2,8 @@ import {
     createBrowserRouter
   } from "react-router-dom";
 import App from "../App";
+import Home from '../pages/home/Home';
+
 const router = createBrowserRouter([
     {
       path: "/",
